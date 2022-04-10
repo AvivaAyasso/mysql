75c98c7dac7040e592b37043d878af56e650b570
aviva <avivaayasso@icloud.com>
2022-04-10 16:00:40 +0300

    mysql

---
+++ connect.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="aviva1234!",
+  database="sakila"
+)
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
+
+#--------------------------------------------
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM actor")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
